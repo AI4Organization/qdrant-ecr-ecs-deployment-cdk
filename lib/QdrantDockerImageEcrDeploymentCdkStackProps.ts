1194faedba34d02424afd97b98db65ec2115ac08
KennyDizi <xamarindevelopervietnam@outlook.com>
2023-12-03 21:00:00 +0700

    Fix read env

--- lib/QdrantDockerImageEcrDeploymentCdkStackProps.ts
+++ lib/QdrantDockerImageEcrDeploymentCdkStackProps.ts
@@ -3,6 +3,6 @@ import * as cdk from 'aws-cdk-lib';
 export interface QdrantDockerImageEcrDeploymentCdkStackProps extends cdk.StackProps {
   readonly repositoryName: string;
   readonly appName: string;
-  imageVersion?: string;
-  environment?: string;
+  readonly imageVersion: string;
+  readonly environment: string;
 }
